46399616fce1aaeb30ada221c63c28cd23ce4a40
Anna <107703043+AnnaTeterina@users.noreply.github.com>
2023-10-03 21:23:48 +0500

    Update revertString.js

--- src/utils/revertString.js
+++ src/utils/revertString.js
@@ -1,8 +1,7 @@
-export function revertString(str) {
-  let revertedStr = "";
-
-  for (let i = str.length - 1; i >= 0; i--) {
-    revertedStr += str[i];
-  }
-  return revertedStr;
+export function reverseString(str) {
+    let result = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+    return result;
 }
