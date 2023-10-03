28f163a23db0d11ee8323ec3b53aaa8200e28795
Anna <107703043+AnnaTeterina@users.noreply.github.com>
2023-10-03 21:15:52 +0500

    Update index.test.js

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,12 +1,20 @@
-import { multiply } from "../index.js";
+import { muliply, sum } from '../index.js';
 
-xdescribe("test multiply", () => {
-  it("multiply 1 * 0 to equal 0", () => {
-    const result = multiply(1, 0);
-    expect(result).toBe(0);
-  }),
-    it("multiply 1 * 1 to equal 1", () => {
-      const result = multiply(1, 1);
-      expect(result).toBe(1);
+describe('test multiply', () => {
+    it('muliply 1 * 0 to equal 0', () => {
+        const result = muliply(1, 0);
+        expect(result).toBe(0);
+    }),
+    
+    it('muliply 1 * 1 to equal 1', () => {
+        const result = muliply(1, 1);
+        expect(result).toBe(1);
+    });
+});
+
+describe('test sum', () => {
+    it('adds 1 + 2 to equal 3', () => {
+        const result = sum(1, 2)
+        expect(result).toBe(3);
     });
 });
